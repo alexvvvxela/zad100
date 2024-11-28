203edae54c0eff9a727f541ade8aae2d00fa95ce
alexvvvxela <158572564+alexvvvxela@users.noreply.github.com>
2024-11-28 18:15:17 +0300

    Update UniqueTest.php

--- tests/UniqueTest.php
+++ tests/UniqueTest.php
@@ -1,42 +1,42 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use alexv\zad10tests\Uniquet;
+use alexv\zad10tests\ArrayUnique;
 
 
 class ArrayUniqueTest extends TestCase
 {
-    public function arrayUniqueTest1()
+    public function ArrayUniqueTest1()
     {
         $input = ['apple', 'banana', 'apple', 'orange', 'banana'];
         $arrayUnique = new ArrayUnique($input);
-        
+
         $expected = ['apple', 'banana', 'orange'];
         $result = $arrayUnique->arrayUnique();
-        
+
         $this->assertEquals($expected, $result);
     }
 
-    public function arrayUniqueOne()
+    public function ArrayUniqueOne()
     {
         $input = ['apple', 'banana', 'orange'];
         $arrayUnique = new ArrayUnique($input);
-        
+
         $expected = ['apple', 'banana', 'orange'];
         $result = $arrayUnique->arrayUnique();
-        
+
         $this->assertEquals($expected, $result);
     }
 
 
-    public function arrayUniqueTest3()
+    public function ArrayUniqueTest3()
     {
         $input = [1, 2, 1, 2, 3];
         $arrayUnique = new ArrayUnique($input);
-        
+
         $expected = [1, 2, 3];
         $result = $arrayUnique->arrayUnique();
-        
+
         $this->assertEquals($expected, $result);
     }
 }
